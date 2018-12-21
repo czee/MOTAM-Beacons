a7295042d30fe1b5593916cac2a198c7fcb446e3
Christopher Zee <me@christopherz.ee>
2018-12-21 13:33:14 +0100

    Remove unneeded files
    Add new sample code and SoftDevice

--- nRF52/intelligent_traffic_light/main.c
+++ nRF52/intelligent_traffic_light/main.c
@@ -56,7 +56,7 @@ static uint8_t current_state_duration ();
 // ======== BLE configuration ========
 
 // MOTAM Beacon static parameters
-#define STATIC_PARAMETERS_LENGTH		0x12									// Length of ADV_DATA_TYPE, MOTAM_ID, LATITUDE AND LONGITUDE
+#define STATIC_PARAMETERS_LENGTH		0x50									// Length of ADV_DATA_TYPE, MOTAM_ID, LATITUDE AND LONGITUDE
 #define ADV_DATA_TYPE					0xFF									// Advertising data type (0xFF -> Manufacturer specific data)
 #define MOTAM_ID                   		0xBE, 0xDE								// MOTAM identifier (DEphisit BEacon in little endian)
 #define LATITUDE						0xA9, 0xDA, 0xE3, 0x41					// GPS latitude of the beacon (float in little endian)
@@ -67,10 +67,12 @@ static uint8_t current_state_duration ();
 #define DEFAULT_STATE					0xFF									// Current state (0x00 red, 0x01 yellow, 0x02 green, 0xFF undeterminated)
 #define DEFAULT_TIMEOUT					0x00									// Time left for next state (0x00 -> no timeout)
 
+#define BYTES_77                                0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f, 0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2a, 0x2b, 0x2c, 0x2d, 0x2e, 0x2f, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3a, 0x3b, 0x3c, 0x3d, 0x3e, 0x3f, 0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4a, 0x4b, 0x4c, 0x4d
+
 // BLE configuration parameters
 #define APP_BLE_CONN_CFG_TAG			1										// Tag that identifies the BLE configuration of the SoftDevice
 #define APP_BLE_OBSERVER_PRIO			3										// BLE observer priority of the application. There is no need to modify this value
-#define ADV_INTERVAL    				MSEC_TO_UNITS(100, UNIT_0_625_MS)  		// The advertising interval for non-connectable advertisement
+#define ADV_INTERVAL    				MSEC_TO_UNITS(400, UNIT_0_625_MS)  		// The advertising interval for non-connectable advertisement
 
 static ble_gap_adv_params_t 			m_adv_params;							// Parameters to be passed to the stack when starting advertising
 static ble_gap_adv_data_t 				m_adv_data;								// Struct that contains pointers to the BLE encoded advertising data
@@ -83,13 +85,7 @@ static uint8_t frame [] =														// Intelligent traffic light default fram
 		STATIC_PARAMETERS_LENGTH,
 		ADV_DATA_TYPE,
 		MOTAM_ID,
-		LATITUDE,
-		LONGITUDE,
-		DIRECTION_FROM,
-		DIRECTION_TO,
-		BEACON_TYPE,
-		DEFAULT_STATE,
-		DEFAULT_TIMEOUT
+		BYTES_77
 	};
 
 // BLE events handler declaration
@@ -130,8 +126,8 @@ static void advertising_init(void)
 	m_adv_params.duration        = BLE_GAP_ADV_TIMEOUT_GENERAL_UNLIMITED;   	// Never time out
 	m_adv_params.max_adv_evts	 = 0;											// No limit of advertising events
 	m_adv_params.filter_policy   = BLE_GAP_ADV_FP_ANY;							// Allow scan request from any device
-	m_adv_params.primary_phy 	 = BLE_GAP_PHY_CODED;							// Long range codification
-	m_adv_params.secondary_phy 	 = BLE_GAP_PHY_CODED;							// Long range codification
+	m_adv_params.primary_phy 	 = BLE_GAP_PHY_1MBPS;							// Long range codification
+	m_adv_params.secondary_phy 	 = BLE_GAP_PHY_1MBPS;							// Long range codification
 
 	// Set the advertising data
 	m_adv_data.adv_data.len = 0;												// No data on frame still
@@ -475,14 +471,14 @@ int main(void)
     clock_init();
     timers_init();
     power_management_init();
-    gpio_init();
+    //gpio_init();
     ble_stack_init();
     advertising_init();
     radio_notification_init();
 
     // Start execution.
     lastState = YELLOW_STATE;													// Initial traffic light state
-    timers_start();
+    //timers_start();
     advertising_start();
     print_beacon_info();
 
